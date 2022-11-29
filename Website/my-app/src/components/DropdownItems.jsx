dcba0f7e186dee2793bb3e62e6bc2af41591ed50
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-11-29 15:39:34 +0530

    Restyle Quick Links

---
+++ Website/my-app/src/components/DropdownItems.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import getQueries from "../query";
+
+function DropdownItems() {
+  // Getting the queries from the query.js file
+  const queries = getQueries();
+
+  // Creating the dropdown items from the queries
+  const itemList = queries.map((query) => {
+    return (
+      <Dropdown.Item href={`#query-${query.id}`}>
+        {`Query ${query.id}`}
+      </Dropdown.Item>
+    );
+  });
+
+  return itemList;
+}
+
+export default DropdownItems;
--- Website/my-app/src/components/QuickLinks.jsx
+++ Website/my-app/src/components/QuickLinks.jsx
@@ -1,28 +1,47 @@
 import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownItems from "./DropdownItems";
 
 function QuickLinks() {
   return (
     <main>
-      {/* Quick Links */}
-      <h2 id="TitleQuick">Quick Links</h2>
-      <div id="QuickLinksBox">
-        <ol class="QueriesList">
-          <li>
-            <a href="#Query1">Bachelors_State_Gov_Details</a>
-          </li>
-          <li>
-            <a href="#Query2">Divorcee Details</a>
-          </li>
-          <li>
-            <a href="#Query3">Own_Child_White_Details</a>
-          </li>
-          <li>
-            <a href="#Query4">Widowed_{">"}50K_Salary_Details</a>
-          </li>
-        </ol>
-      </div>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Quick Links Button
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <DropdownItems />
+        </Dropdown.Menu>
+      </Dropdown>
     </main>
   );
 }
 
 export default QuickLinks;
+
+// Old code:
+// function QuickLinks() {
+//   return (
+//     <main>
+//       {/* Quick Links */}
+//       <h2 id="TitleQuick">Quick Links</h2>
+//       <div id="QuickLinksBox">
+//         <ol class="QueriesList">
+//           <li>
+//             <a href="#Query1">Bachelors_State_Gov_Details</a>
+//           </li>
+//           <li>
+//             <a href="#Query2">Divorcee Details</a>
+//           </li>
+//           <li>
+//             <a href="#Query3">Own_Child_White_Details</a>
+//           </li>
+//           <li>
+//             <a href="#Query4">Widowed_{">"}50K_Salary_Details</a>
+//           </li>
+//         </ol>
+//       </div>
+//     </main>
+//   );
+// }
