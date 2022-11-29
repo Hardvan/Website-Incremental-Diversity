03efc547b35780dde8622787291bcb64e38216db
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-11-29 12:23:46 +0530

    Create Query.jsx

--- Website/my-app/src/components/App.jsx
+++ Website/my-app/src/components/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "../styles.css";
 // Importing Components
 import Headers from "./Headers";
+import Query from "./Query";
 
 function App() {
   return (
     <div className="App">
       <Headers />
+      <Query />
     </div>
   );
 }
