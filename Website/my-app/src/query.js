40f62329a1b79e5b2fc42b8ddd115ceb7f5d97b8
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-11-29 12:45:32 +0530

    Make query.js hold two objects for testing

--- Website/my-app/src/query.js
+++ Website/my-app/src/query.js
@@ -1,4 +1,5 @@
 function getQueries() {
+  // Only two objects for now
   let queries = [
     {
       title: "Query 1",
@@ -9,6 +10,16 @@ function getQueries() {
       caption: "Fig 1: Caption",
       explanation:
         "It can be a lot of fun simply learning interesting facts and that's exactly what this online tool will allow you to do. If you spend even a little time, you're going to quickly expand your knowledge with a lot of random fun facts. Even if you never share them with others (which you likely will at some point), it's fun to know you have that knowledge in your head.",
+    },
+    {
+      title: "Query 2",
+      statement:
+        "SELECT * FROM `plenary-chalice-369413.Incremental_Diversity_Dataset.Masked_Table_1000_Records_k_3` WHERE Marital_Status='Divorced' ORDER BY Group_ID LIMIT 1000",
+      image:
+        "https://tse3.mm.bing.net/th?id=OIP.8SKH5IQ1zavH76joiw0VJwHaHa&pid=Api&P=0",
+      caption: "Fig 2: Caption",
+      explanation:
+        "She sat down with her notebook in her hand, her mind wandering to faraway places. She paused and considered all that had happened. It hadn't gone as expected. When the day began she thought it was going to be a bad one, but as she sat recalling the day's events to write them down, she had to admit, it had been a rather marvelous day.",
     },
   ];
 
