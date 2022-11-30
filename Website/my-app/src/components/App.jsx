f6db761b7a1d7f275fcf37307683c40fd6975935
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-11-30 10:29:07 +0530

    Add About and Footer to website

--- Website/my-app/src/components/App.jsx
+++ Website/my-app/src/components/App.jsx
@@ -3,12 +3,16 @@ import "../styles.css";
 // Importing Components
 import Headers from "./Headers";
 import Queries from "./Queries";
+import About from "./About";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Headers />
       <Queries />
+      <About />
+      <Footer />
     </div>
   );
 }
