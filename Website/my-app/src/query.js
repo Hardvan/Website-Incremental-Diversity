a0aa395773e584a87e9823c21dd606943f2072c8
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-12-02 15:52:13 +0530

    Add query3 and query4

--- Website/my-app/src/query.js
+++ Website/my-app/src/query.js
@@ -2,7 +2,7 @@ function getQueries() {
   let queries = [
     {
       id: 1,
-      title: "State-gov Bachelors",
+      title: "State-gov Bachelors Details",
       statement:
         "SELECT Age,Gender,Zip_Code,Education,Employment FROM `plenary-chalice-369413.Incremental_Diversity_Dataset.Masked_Table_1000_Records_k_3` WHERE Education='Bachelors' AND Employment='State-gov' ORDER BY Group_ID LIMIT 1000",
       image: require("./images/Query1_Table.png"),
@@ -21,6 +21,28 @@ function getQueries() {
       explanation:
         "The Age, Relationship, Race and Disease of people who are divorced is shown in the table above. The inference can be made that most of the people are white, currently not-in-family, and lie in the age group 40-69.",
       link: "https://console.cloud.google.com/bigquery?sq=566051837684:6d15e018fb0741f5a47fe11a1a6597c0",
+    },
+    {
+      id: 3,
+      title: "Own-Child-White Details",
+      statement:
+        "SELECT Age,Gender,Zip_Code,Employment FROM `plenary-chalice-369413.Incremental_Diversity_Dataset.Masked_Table_1000_Records_k_3` WHERE Relationship='Own-child' AND Race='White' ORDER BY Group_ID LIMIT 1000",
+      image: require("./images/Query3_Table.png"),
+      caption: "Fig 3: Own child with White Race Masked Table",
+      explanation:
+        "The Age, Gender, Zip Code and Employment of people who are white in race and their relationship in the family is own-child is shown in the table above. The conclusion can be drawn that most of the people are between the ages of 20 and 40 and predominantly have Private employment.",
+      link: "https://console.cloud.google.com/bigquery?sq=566051837684:4e7e6232337849df8fd4d097f8c8f8ed",
+    },
+    {
+      id: 4,
+      title: "Widowed-Salary>50K Details",
+      statement:
+        "SELECT Age,Gender,Zip_Code,Education FROM `plenary-chalice-369413.Incremental_Diversity_Dataset.Masked_Table_1000_Records_k_3` WHERE Marital_Status='Widowed' AND Salary='>50K' ORDER BY Group_ID LIMIT 1000",
+      image: require("./images/Query4_Table.png"),
+      caption: "Fig 4: Widowed with Salary>50K Masked Table",
+      explanation:
+        "The Age, Gender, Zip Code and Education of widows who have their salary greater than 50K is shown in the table above. It can be observed that such a criteria is met by a handful of people who have their highest education as High School Graduate.",
+      link: "https://console.cloud.google.com/bigquery?sq=566051837684:5afbe5baac724c1e98c3d79fd7079ff6",
     },
   ];
 
