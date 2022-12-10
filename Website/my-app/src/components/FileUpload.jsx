d4ad5e1c7d08149cc6b55dac844f57ac9a5671e1
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-12-10 11:20:24 +0530

    Groundwork for file upload server

--- Website/my-app/src/components/FileUpload.jsx
+++ Website/my-app/src/components/FileUpload.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 function FileUpload() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  };
+
+  const handleSubmission = (event) => {
+    // Prevent default behavior of page refreshing
+    event.preventDefault();
+
+    if (!isFilePicked) {
+      alert("Please select a file to upload first!");
+    } else {
+      const url = "http://localhost:3000/upload"; // TODO: Change when hosting on GitHub
+      const formData = new FormData();
+      formData.append("FileReceived", selectedFile);
+      formData.append("FileName", selectedFile.name);
+      const config = {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      };
+
+      axios.post(url, formData, config).then((response) => {
+        console.log(response.data);
+      });
+    }
+  };
+
   return (
     <div id="fileupload">
       <h2 className="SectionTitle" id="FileUploadTitle">
@@ -10,7 +42,6 @@ function FileUpload() {
       </h2>
       {/* Guidelines */}
       <Card border="primary" style={{ width: "75%" }} className="CenterCard">
-        {/* Original Dataset */}
         <Card.Body>
           <Card.Title className="CardTitle">
             Guidelines for the dataset:
@@ -32,23 +63,40 @@ function FileUpload() {
             </ul>
           </Card.Text>
 
+          {/* File Upload Section */}
           <div class="ChooseFile">
             <p>Click on the "Choose File" button to upload your dataset:</p>
             {/* TODO: Add route */}
-            <form action="/">
-              <fieldset>
-                <label for="FileUpload">Select a CSV File:</label>
-                <input
-                  type="file"
-                  name="FileReceived"
-                  id="FileReceived"
-                  accept=".csv,.xlsx"
-                  value=""
-                />
-                <br />
-                <input type="submit" id="SubmitFile" name="SubmitFile" />
-              </fieldset>
-            </form>
+            <div>
+              <input
+                type="file"
+                name="FileReceived"
+                id="FileReceived"
+                accept=".csv,.xlsx"
+                onChange={changeHandler}
+              />{" "}
+              {isFilePicked ? (
+                <div>
+                  <p>Filename: {selectedFile.name}</p>
+                  <p>Filetype: {selectedFile.type}</p>
+                  <p>Size: {selectedFile.size} bytes</p>
+                </div>
+              ) : (
+                <p style={{ color: "#808080" }}>
+                  (Select a file to show details)
+                </p>
+              )}
+              <Button
+                variant="success"
+                href=""
+                className="CustomButton"
+                type="submit"
+                name="SubmitFile"
+                onClick={handleSubmission}
+              >
+                Submit
+              </Button>
+            </div>
           </div>
         </Card.Body>
       </Card>
@@ -56,44 +104,5 @@ function FileUpload() {
     </div>
   );
 }
-
-// Old Code:
-// return (
-//   <div id="fileupload">
-//     <h2 className="SectionTitle">Check with your Own Dataset:</h2>
-//     {/* Guidelines */}
-//     <h3>Guidelines for the dataset:</h3>
-//     <ol>
-//       <li>
-//         The most sensitive attribute should be considered as primary sensitive
-//         attribute. The second most sensitive attribute should be considered as
-//         secondary sensitive attribute, and so on.
-//       </li>
-//       <li>The quasi-identifiers will be either generalised or masked.</li>
-//       <li>The dataset should be in .csv or .xlsx format.</li>
-//     </ol>
-//     {/* Upload Section */}
-//     {/* TODO: Style Upload Section */}
-//     <div class="ChooseFile">
-//       <strong>
-//         <p>Click on the "Choose File" button to upload your dataset:</p>
-//       </strong>
-//       {/* TODO: Add route */}
-//       <form action="/">
-//         <fieldset>
-//           <label for="FileUpload">Select a CSV File:</label>
-//           <input
-//             type="file"
-//             name="FileReceived"
-//             id="FileReceived"
-//             accept=".csv,.xlsx"
-//           />
-//           <br />
-//           <input type="submit" id="SubmitFile" name="SubmitFile" />
-//         </fieldset>
-//       </form>
-//     </div>
-//   </div>
-// );
 
 export default FileUpload;
